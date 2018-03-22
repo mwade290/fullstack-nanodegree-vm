0c241a4c0c58a8f0509f425be3609425103709b0
= <=>
2018-03-22 16:02:39 +0000

    Added JSON, login/logout - working

--- vagrant/catalog/README.txt
+++ vagrant/catalog/README.txt
@@ -1,3 +1,5 @@
 References:
 
 https://stackoverflow.com/questions/32567100/add-a-nav-bar-to-all-templates
+http://jinja.pocoo.org/docs/2.10/templates/
+https://blackrockdigital.github.io/startbootstrap-simple-sidebar/
--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for
+from flask import Flask, render_template, request, redirect, jsonify, url_for, make_response, flash
+from flask import session as login_session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
 import sys
+import random
+import string
+import httplib2
+import json
+import requests
 sys.path.insert(0, 'Modules')
 from database_setup import Base, Country, Highlight
 
 app = Flask(__name__)
 app.secret_key = 'super_secret_key'
+
+CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Catalog App"
 
 engine = create_engine('sqlite:///countries.db')
 Base.metadata.bind = engine
@@ -14,7 +25,140 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+# Login
+@app.route('/login')
+def showLogin():
+	state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+					for x in xrange(32))
+	login_session['state'] = state
+	countries = session.query(Country).order_by(Country.name).all()
+	return render_template('login.html', countries=countries, STATE=state)
+
+# Logout
+@app.route('/logout')
+def disconnect():
+	if 'username' in login_session:
+		gdisconnect()
+		flash("Log out successful")
+	else:
+		flash("You were not logged in.")
+	return redirect(url_for('countries'))
+
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+	# Validate state token
+	if request.args.get('state') != login_session['state']:
+		response = make_response(json.dumps('Invalid state parameter.'), 401)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+	# Obtain authorization code
+	code = request.data
+
+	try:
+		# Upgrade the authorization code into a credentials object
+		oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+		oauth_flow.redirect_uri = 'postmessage'
+		credentials = oauth_flow.step2_exchange(code)
+	except FlowExchangeError:
+		response = make_response(json.dumps('Failed to upgrade the authorization code.'), 401)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+
+	# Check that the access token is valid.
+	access_token = credentials.access_token
+	url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+	h = httplib2.Http()
+	result = json.loads(h.request(url, 'GET')[1])
+	# If there was an error in the access token info, abort.
+	if result.get('error') is not None:
+		response = make_response(json.dumps(result.get('error')), 500)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+
+	# Verify that the access token is used for the intended user.
+	gplus_id = credentials.id_token['sub']
+	if result['user_id'] != gplus_id:
+		response = make_response(json.dumps("Token's user ID doesn't match given user ID."), 401)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+
+	# Verify that the access token is valid for this app.
+	if result['issued_to'] != CLIENT_ID:
+		response = make_response(json.dumps("Token's client ID does not match app's."), 401)
+		print("Token's client ID does not match app's.")
+		response.headers['Content-Type'] = 'application/json'
+		return response
+
+	stored_access_token = login_session.get('access_token')
+	stored_gplus_id = login_session.get('gplus_id')
+	if stored_access_token is not None and gplus_id == stored_gplus_id:
+		response = make_response(json.dumps('Current user is already connected.'),
+								 200)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+
+	# Store the access token in the session for later use.
+	login_session['access_token'] = credentials.access_token
+	login_session['gplus_id'] = gplus_id
+
+	# Get user info
+	userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+	params = {'access_token': credentials.access_token, 'alt': 'json'}
+	answer = requests.get(userinfo_url, params=params)
+
+	data = answer.json()
+
+	login_session['username'] = data['name']
+	login_session['picture'] = data['picture']
+	login_session['email'] = data['email']
+
+	output = ''
+	output += '<h1>Welcome, '
+	output += login_session['username']
+	output += '!</h1>'
+	#output += '<img src="'
+	#output += login_session['picture']
+	#output += ' " style = "width: 300px; height: 300px;border-radius:
+	#150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+	flash("you are now logged in as %s" % login_session['username'])
+	print("done!")
+	return output
+
+	# DISCONNECT - Revoke a current user's token and reset their login_session
+@app.route('/gdisconnect', methods=['POST'])
+def gdisconnect():
+	access_token = login_session.get('access_token')
+	if access_token is None:
+		print('Access Token is None')
+		response = make_response(json.dumps('Current user not connected.'), 401)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+	print('In gdisconnect access token is %s'), access_token
+	print('User name is: ')
+	print(login_session['username'])
+	url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+	h = httplib2.Http()
+	result = h.request(url, 'GET')[0]
+	print('result is ')
+	print(result)
+	if result['status'] == '200':
+		del login_session['access_token']
+		del login_session['gplus_id']
+		del login_session['username']
+		del login_session['email']
+		del login_session['picture']
+		response = make_response(json.dumps('Successfully disconnected.'), 200)
+		response.headers['Content-Type'] = 'application/json'
+		flash("Successfully disconnected.")
+		return response
+	else:
+		response = make_response(json.dumps('Failed to revoke token for given user. %s' % result['status'], 400))
+		response.headers['Content-Type'] = 'application/json'
+		return response
+
 # Show all countries in serialised format
+@app.route('/JSON')
 @app.route('/countries/JSON')
 def countriesJSON():
 	countries = session.query(Country).order_by(Country.name).all()
@@ -25,15 +169,21 @@ def countriesJSON():
 @app.route('/countries')
 def countries():
 	countries = session.query(Country).order_by(Country.name).all()
-	return render_template('countries.html', countries=countries)
-	
+	return render_template('countries.html', countries=countries, session=login_session)
+
+# Show all country highlights in serialised format
+@app.route('/countries/<country_name>/highlights/JSON')
+def countryHighlightsJSON(country_name):
+	country = session.query(Country).filter_by(name=country_name).first()
+	highlights = session.query(Highlight).filter_by(country_id=country.id).all()
+	return jsonify(highlights=[h.serialize for h in highlights])
+
 # Show all country highlights
 @app.route('/countries/<country_name>/highlights')
 def countryHighlights(country_name):
 	countries = session.query(Country).order_by(Country.name).all()
 	country = session.query(Country).filter_by(name=country_name).first()
-	highlights = session.query(Highlight).filter_by(
-		country_id=country.id).all()
+	highlights = session.query(Highlight).filter_by(country_id=country.id).all()
 	return render_template('countryHighlights.html', countries=countries, highlights=highlights, country=country)
 	
 # Add new country
@@ -47,7 +197,7 @@ def addNewCountry():
 			country = Country(name=newName)
 			session.add(country)
 			session.commit()
-			return redirect(url_for('countries'))
+			return redirect(url_for('countryHighlights', country_name=country.name))
 		else:
 			return error(message)
 	else:
@@ -61,6 +211,9 @@ def deleteCountry(country_name):
 	country = session.query(Country).filter_by(name=country_name).first()
 	if country > 0:
 		if request.method == 'POST':
+			highlights = session.query(Highlight).filter_by(country_id=country.id).all()
+			for highlight in highlights:
+				session.delete(highlight)
 			session.delete(country)
 			session.commit()
 			return redirect(url_for('countries'))
@@ -80,6 +233,18 @@ def addCountryConditions(name):
 	else:
 		return ''
 	
+# Show country highlight description in serialised format
+@app.route('/countries/<country_name>/highlights/<highlight_name>/description/JSON')
+def highlightDescriptionJSON(country_name, highlight_name):
+	country = session.query(Country).filter_by(name=country_name).first()
+	highlight = session.query(Highlight).filter_by(country_id=country.id,
+		name=highlight_name).first()
+	if country > 0 and highlight > 0:
+		countries = session.query(Country).order_by(Country.name).all()
+		return jsonify(highlight=highlight.serialize)
+	else:
+		return error('Unable to find entry in database.')
+
 # Show description for highlight
 @app.route('/countries/<country_name>/highlights/<highlight_name>/description')
 def highlightDescription(country_name, highlight_name):
